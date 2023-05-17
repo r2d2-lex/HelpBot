88fa579885a73c25daba65fbd9a57ba7b9c28c98
Gogs <r2d2-lex@yandex.ru>
2023-05-17 08:53:23 +0500

    небольшой рефактор

---
+++ weather/weather.py
@@ -0,0 +1,19 @@
+import logging
+from aiorequest import Service, fetch_data
+
+
+async def fetch_weather_from_service(service: Service) -> str:
+    weather = 'Ошибка получение результата'
+    weather_params_dict = service.params
+    result_dict = await fetch_data(service)
+    logging.debug(result_dict)
+    try:
+        weather_string = ''
+        for key, value in weather_params_dict.items():
+            param_value = result_dict[value['param']]
+            param_value = str(param_value)
+            weather_string += value['description'] + ': ' + param_value + ' ' + value.get('sign', '') + '\r\n'
+        weather = weather_string
+    except (KeyError, TypeError) as error:
+        logging.error(f'Ошибка получения данных: {error}')
+    return weather
--- weather/weather_api.py
+++ weather/weather_api.py
@@ -2,43 +2,29 @@ from collections import OrderedDict
 import asyncio
 import sys
 sys.path.append("..")
-from aiorequest import fetch_data, Service
+from aiorequest import Service
+from weather import fetch_weather_from_service
 from config import logging, WEATHER_API_KEY, CITY
 
 
-service = Service(
-        'weatherapi.com',
-        f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={CITY}&aqi=no',
-        'current'
-)
-
-weather_dict = OrderedDict(
+weather_api_dict = OrderedDict(
     temp_c=dict(param='temp_c', description='Температура воздуха', sign='°C'),
     feels_like_c=dict(param='feelslike_c', description='Ощущается как', sign='°C'),
     wind_kph=dict(param='wind_kph', description='Скорость ветра', sign='Км/ч'),
     last_updated=dict(param='last_updated', description='Данные получены'),
 )
 
-
-async def fetch_weather_from_weather_api() -> str:
-    weather = 'Ошибка получение результата'
-    result_dict = await fetch_data(service)
-    logging.info(result_dict)
-    try:
-        weather_string = ''
-        for key, value in weather_dict.items():
-            param_value = result_dict[value['param']]
-            param_value = str(param_value)
-            weather_string += value['description'] + ': ' + param_value + ' ' + value.get('sign', '') + '\r\n'
-        weather = weather_string
-    except (KeyError, TypeError) as error:
-        logging.error(f'Ошибка получения данных: {error}')
-    return weather
+service_weather_api = Service(
+    'weatherapi.com',
+    f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={CITY}&aqi=no',
+    'current',
+    weather_api_dict,
+)
 
 
 def main():
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(fetch_weather_from_weather_api())
+    result = loop.run_until_complete(fetch_weather_from_service(service_weather_api))
     loop.close()
     print(result)
 
