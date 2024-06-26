c8ae89edb5d20f787d5f7cbdb2b3f555df14f201
Gogs <r2d2-lex@yandex.ru>
2024-06-26 09:28:13 +0500

    Аналог depends из FastApi + read_news из БД

--- crowler/crud.py
+++ crowler/crud.py
@@ -8,6 +8,14 @@ sys.path.append("..")
 from models.habr import News as ModelNews
 from schema.habr import HabrArt as SchemaHabrArt
 
+
+async def read_news(db:AsyncSession) -> list[SchemaHabrArt]:
+    logging.info('Read news from db...')
+    query = select(ModelNews)
+    result = await db.execute(query)
+    return list(result.scalars().all())
+
+
 async def create_news(db: AsyncSession, item: SchemaHabrArt):
     logging.info('Create news record...')
     db_item = ModelNews(
--- crowler/habr.py
+++ crowler/habr.py
@@ -2,7 +2,7 @@ import logging
 from bs4 import BeautifulSoup
 import asyncio
 import sys
-from crud import create_news
+from crud import create_news, read_news
 from db import get_db
 sys.path.append("..")
 from aiorequest import fetch_text
@@ -16,17 +16,31 @@ HABR_ROOT_URL = 'https://habr.com'
 async def get_habr_news():
     return await fetch_text(HABR_NEWS_URL)
 
-async def write_db(news):
-    db = get_db()
-    db_ = await db.__anext__()
-    await create_news(db_, news)
+def depends(func):
+    async def wrapped(*args, **kwargs):
+        db = get_db()
+        db_ = await db.__anext__()
+        return await func(db_, *args, **kwargs)
+    return wrapped
 
+def print_news(news: HabrArt):
+    print(f'{news.news_id}\r\n'
+          f'{news.title}\r\n'
+          f'{news.url}\r\n'
+          f'{news.content}\r\n'
+          f'{news.published}\r\n'
+          )
 
-async def main():
-    with open('example.html', 'r') as file_descr:
-        result = file_descr.read()
+@depends
+async def write_news_in_db(db_session, news):
+    await create_news(db_session, news)
 
-    soup = BeautifulSoup(result, 'html.parser')
+@depends
+async def read_news_from_db(db_session):
+    return await read_news(db_session)
+
+async def parse_article(data):
+    soup = BeautifulSoup(data, 'html.parser')
     articles = soup.findAll(class_='tm-articles-list__item')
 
     for article in articles:
@@ -39,17 +53,20 @@ async def main():
                 published = get_date_time('%Y-%m-%d %H:%M:%S'),
                 image = bytearray()
             )
-            print(f'{news.news_id}\r\n'
-                  f'{news.title}\r\n'
-                  f'{news.url}\r\n'
-                  f'{news.content}\r\n'
-                  f'{news.published}\r\n'
-                  )
-            print('\r\n')
-            await write_db(news)
+            print_news(news)
+            await write_news_in_db(news)
         except AttributeError as err:
             logging.info(f'{err}')
             continue
+
+
+async def main():
+    # with open('example.html', 'r') as file_descr:
+    #     result = file_descr.read()
+    #     await parse_article(result)
+    news = await read_news_from_db()
+    for news_item in news:
+        print_news(news_item)
 
 
 if __name__ == '__main__':
