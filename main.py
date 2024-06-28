434c5030ce9a0958d018104144a0ab8a98067cd5
Gogs <r2d2-lex@yandex.ru>
2024-06-28 15:51:44 +0500

    Переход на aiogram==3

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import asyncio
+import logging
 from time import sleep
-
-from aiogram import Bot, Dispatcher, types, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ContentTypes, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.exceptions import BadRequest, MessageNotModified
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
 from config import TOKEN, logging
 from utils import get_keyboard
 
@@ -16,25 +14,23 @@ from erates.cbr_xml_daily import get_exchange_rates
 from search_image.google_search_image import google_search_image, google_next_search, google_new_search
 
 
-bot = Bot(TOKEN)
-dp = Dispatcher(bot, storage=MemoryStorage())
-dp.middleware.setup(LoggingMiddleware())
+dp = Dispatcher()
 
 
-@dp.message_handler(commands=('start', 'help'))
+@dp.message(Command('start', 'help', prefix='!/'))
 async def command_start(message: types.Message):
     text = 'Меню функций: /key'
     await message.reply(text, reply_markup=get_keyboard())
 
 
-@dp.callback_query_handler(text='weather_api')
-async def command_weather(callback_query: types.CallbackQuery):
+@dp.callback_query(F.data == 'weather_api')
+async def handler_command_weather(callback_query: types.CallbackQuery):
     result = await get_weather_from_weather_api()
-    await callback_query.message.reply(result, reply_markup=get_keyboard())
-    await callback_query.answer()
+    await callback_query.message.answer(text=result, reply_markup=get_keyboard())
 
-@dp.callback_query_handler(text='habr_news')
-async def get_habr_news(callback_query: types.CallbackQuery):
+
+@dp.callback_query(F.data == 'habr_news')
+async def handler_get_habr_news(callback_query: types.CallbackQuery):
     result = await read_news_from_db()
     for article_item in result:
         message = (f'{article_item.title}\r\n\r\n'
@@ -46,14 +42,14 @@ async def get_habr_news(callback_query: types.CallbackQuery):
     await callback_query.answer()
 
 
-@dp.message_handler(commands='img_next')
+@dp.message(Command('img_next'))
 async def search_image_next(message: types.Message):
     google_next_search(message.from_user.id)
     await send_image(message)
     await message.reply('Следующие сообщения', reply_markup=get_keyboard())
 
 
-@dp.message_handler(commands='img')
+@dp.message(Command('img'))
 async def search_image(message: types.Message):
     google_new_search(message.from_user.id)
     await send_image(message)
@@ -64,60 +60,65 @@ async def send_image(message: types.Message):
     search_arg = message.get_args()
     logging.debug(f'Аргументы: {search_arg}')
     for image in google_search_image(search_arg, message.from_user.id, 5):
-        await bot.send_photo(chat_id=message.chat.id, photo=image)
+        await message.bot.send_photo(chat_id=message.chat.id, photo=image)
 
 
-@dp.callback_query_handler(text='open_weather')
-async def command_weather(callback_query: types.CallbackQuery):
+@dp.callback_query(F.data == 'open_weather')
+async def handler_command_weather(callback_query: types.CallbackQuery):
     result = await get_weather_from_open_weather()
     await callback_query.message.reply(result, reply_markup=get_keyboard())
     await callback_query.answer()
 
 
-@dp.callback_query_handler(text='exchange_rates')
-async def exchange_rates(callback_query: types.CallbackQuery):
+@dp.callback_query(F.data == 'exchange_rates')
+async def handler_exchange_rates(callback_query: types.CallbackQuery):
     result = await get_exchange_rates()
     await callback_query.message.reply(result, reply_markup=get_keyboard())
     await callback_query.answer()
 
 
-@dp.message_handler(commands='key')
+@dp.message(Command('key'))
 async def send_inline_keyboard(message: types.Message):
     await message.reply('Выбери команду:', reply_markup=get_keyboard())
 
 
-@dp.callback_query_handler(text='remove')
+@dp.callback_query(F.data == 'remove')
 async def remove_inline_keyboard(callback_query: types.CallbackQuery):
     await callback_query.answer('Removing kb..')
     await callback_query.message.edit_text('Выполните /key для вызова меню функций...')
 
 
-@dp.callback_query_handler()
+@dp.callback_query()
 async def handle_all_callback_queries(callback_query: types.CallbackQuery):
     await callback_query.answer()
 
 
-@dp.message_handler()
+@dp.message()
 async def echo_message(message: types.Message):
     await message.reply(message.text)
 
 
-@dp.message_handler(content_types=ContentTypes.STICKER)
-async def echo_sticker(message: types.Message):
-    await message.reply_sticker(message.sticker.file_id, reply=False)
+# @dp.message(content_types=ContentTypes.STICKER)
+# async def echo_sticker(message: types.Message):
+#     await message.reply_sticker(message.sticker.file_id, reply=False)
+#
+#
+# @dp.errors(exception=MessageNotModified)
+# async def handler_error_message_not_modified(update: types.Update, exception):
+#     logging.info('Not modified update is %s', update)
+#     return True
+#
+#
+# @dp.errors(exception=TelegramBadRequest)
+# async def handler_error_message_bad_request(update: types.Update, exception):
+#     logging.info('Message must be non-empty %s %s', update)
+#     return True
 
-
-@dp.errors_handler(exception=MessageNotModified)
-async def handler_error_message_not_modified(update: types.Update, exception):
-    logging.info('Not modified update is %s', update)
-    return True
-
-
-@dp.errors_handler(exception=BadRequest)
-async def handler_error_message_bad_request(update: types.Update, exception):
-    logging.info('Message must be non-empty %s %s', update)
-    return True
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(TOKEN)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    asyncio.run(main())
--- utils.py
+++ utils.py
@@ -1,19 +1,17 @@
 import datetime
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_keyboard():
-    kb = InlineKeyboardMarkup()
-    url_btn = InlineKeyboardButton('Yandex', url='https://ya.ru')
-    remove_btn = InlineKeyboardButton('Удалить кнопки', callback_data='remove')
-    weather_api_btn = InlineKeyboardButton('weatherapi', callback_data='weather_api')
-    open_weather_btn = InlineKeyboardButton('openweather', callback_data='open_weather')
-    exchange_rates_btn = InlineKeyboardButton('Курсы валют', callback_data='exchange_rates')
-    news_btn = InlineKeyboardButton('Habr новости', callback_data='habr_news')
-    kb.add(url_btn, weather_api_btn, open_weather_btn, exchange_rates_btn, remove_btn,
-           news_btn,
-           )
-    return kb
+    builder = InlineKeyboardBuilder()
+    builder.button(text='Yandex', url='https://ya.ru')
+    builder.button(text='Удалить кнопки', callback_data='remove')
+    builder.button(text='weatherapi', callback_data='weather_api')
+    builder.button(text='openweather', callback_data='open_weather')
+    builder.button(text='Курсы валют', callback_data='exchange_rates')
+    builder.button(text='Habr новости', callback_data='habr_news')
+    builder.adjust(2, 3)
+    return builder.as_markup()
 
 def get_date_time(template):
     return datetime.datetime.now(tz=datetime.timezone.utc).strftime(template)
